9b7fef94122f25d521c52ccc13f4d6eda6c43a88
Boyan Rabchev <boyan@rabchev.com>
2014-09-22 01:10:07 +0300

    Open Dialog styles

--- nls/root/strings.js
+++ nls/root/strings.js
@@ -1,5 +1,6 @@
 define({
-    BUTTON_OPEN:    "Open",
-    BUTTON_SAVE:    "Save",
-    BUTTON_CANCEL:  "Cancel"
+    BUTTON_OPEN         : "Open",
+    BUTTON_SAVE         : "Save",
+    BUTTON_CANCEL       : "Cancel",
+    BUTTON_OPEN_FOLDER  : "Click to list the contents of this folder."
 });
