4f93d731892f336af66cace92a9100a3f6a823fc
Boyan Rabchev <boyan@rabchev.com>
2014-09-26 18:53:22 +0300

    Save File Dialog

--- nls/root/strings.js
+++ nls/root/strings.js
@@ -2,5 +2,6 @@ define({
     BUTTON_OPEN         : "Open",
     BUTTON_SAVE         : "Save",
     BUTTON_CANCEL       : "Cancel",
-    BUTTON_OPEN_FOLDER  : "List contents"
+    BUTTON_OPEN_FOLDER  : "List contents",
+    LABEL_FILE_NAME     : "Name:"
 });
