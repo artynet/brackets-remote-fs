a34454526beb381c3cb42506cde26e956a3d476a
Boyan Rabchev <boyan@rabchev.com>
2014-09-29 21:16:53 +0300

    Create Folder button for save dialog.

--- nls/root/strings.js
+++ nls/root/strings.js
@@ -1,7 +1,8 @@
 define({
-    BUTTON_OPEN         : "Open",
-    BUTTON_SAVE         : "Save",
-    BUTTON_CANCEL       : "Cancel",
-    BUTTON_OPEN_FOLDER  : "List contents",
-    LABEL_FILE_NAME     : "Name:"
+    BUTTON_OPEN             : "Open",
+    BUTTON_SAVE             : "Save",
+    BUTTON_CANCEL           : "Cancel",
+    BUTTON_OPEN_FOLDER      : "List contents",
+    BUTTON_CREATE_FOLDER    : "Create Folder ...",
+    LABEL_FILE_NAME         : "Name:"
 });
