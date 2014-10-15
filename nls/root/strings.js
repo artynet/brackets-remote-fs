86503190e10fe64d89eda4196d929cb077af6f55
Boyan Rabchev <boyan@rabchev.com>
2014-10-16 00:48:19 +0300

    Confirm replace on file save.

--- nls/root/strings.js
+++ nls/root/strings.js
@@ -5,7 +5,10 @@ define({
     BUTTON_OPEN_FOLDER      : "List contents",
     BUTTON_CREATE_FOLDER    : "Create Folder ...",
     BUTTON_OK               : "OK",
+    BUTTON_REPLACE          : "Replace",
     LABEL_FILE_NAME         : "Name:",
     LABEL_FOLDER_NAME       : "Folder Name",
-    MSG_FILE_EXISTS         : "There is already a file with the same name!"
+    MSG_FILE_EXISTS         : "There is already a file with the same name!",
+    MSG_CONFIRM_REPLACE     : "The file already exists in \"{0}\".  Replacing it will overwrite its contents.",
+    TITLE_CONFIRM_REPLACE   : "A file named \"{0}\" already exists.  Do you want to replace it?"
 });
