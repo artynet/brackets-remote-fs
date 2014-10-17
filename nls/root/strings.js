7da5f695f55ab97c257c0f926305bd3a5554f735
Boyan Rabchev <boyan@rabchev.com>
2014-10-17 18:04:48 +0300

    Save dialog confirmation.

--- nls/root/strings.js
+++ nls/root/strings.js
@@ -9,6 +9,6 @@ define({
     LABEL_FILE_NAME         : "Name:",
     LABEL_FOLDER_NAME       : "Folder Name",
     MSG_FILE_EXISTS         : "There is already a file with the same name!",
-    MSG_CONFIRM_REPLACE     : "The file already exists in \"{0}\".  Replacing it will overwrite its contents.",
-    TITLE_CONFIRM_REPLACE   : "A file named \"{0}\" already exists.  Do you want to replace it?"
+    MSG_CONFIRM_REPLACE     : "<p>The file <strong>\"{0}\"</strong> already exists in \"{1}\".  Replacing it will overwrite its contents.</p> Do you want to replace it?",
+    TITLE_CONFIRM_REPLACE   : "A file with the same name already exists."
 });
